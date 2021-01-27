27365e3da0ff9707d1e103f20d0057f1472c031b
kuznetsov_danil <danya.kuznetsov76@gmail.com>
2021-01-27 23:43:46 +0500

    RR

--- infrastructure/forms.py
+++ infrastructure/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
-from infrastructure.models import Table
 from mini_restaurant.settings import EXIST_TIME
 
 
@@ -8,11 +7,6 @@ class TableForm(forms.Form):
     time = forms.ChoiceField(label="Время бронирования", required=True)
     owner_name = forms.CharField(label="Ваше имя", max_length=15, required=True)
     owner_number = forms.CharField(label="Ваш номер телефона", max_length=12, required=True)
-
-    # @staticmethod
-    # def get_available_table_size():
-    #     available_table = Table.objects.filter(status='free').order_by('size')
-    #     return [table.size for table in available_table]
 
     def __init__(self, *args, **kwargs):
         super(TableForm, self).__init__(*args, **kwargs)
--- mini_restaurant/settings.py
+++ mini_restaurant/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -74,13 +75,15 @@ WSGI_APPLICATION = 'mini_restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+# For heroku
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
