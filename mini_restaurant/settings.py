69209573ded22a3d30a5c0fc052e8447768ef504
kuznetsov_danil <danya.kuznetsov76@gmail.com>
2021-01-28 00:10:36 +0500

    Изменил базу данных на локальную вместо heroku

--- mini_restaurant/settings.py
+++ mini_restaurant/settings.py
@@ -75,15 +75,15 @@ WSGI_APPLICATION = 'mini_restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # For heroku
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+# DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
